d97930141479a3be19eb4d416c53236ef39eee87
Kinga Cholewik <kinga.cholewik007.gmail.com>
2023-09-23 22:23:46 +0200

    ready app to vote

--- main.js
+++ main.js
@@ -17,24 +17,39 @@ voteCounts.set('Opcja 1', 0);
 voteCounts.set('Opcja 2', 0);
 voteCounts.set('Opcja 3', 0);
 
+//userzy, którzy już zagłosowali
 const votedUsers = new Set();
 
 const voting = (e) => {
   e.preventDefault();
-  console.log('glosowanie');
+  const user = userName.value;
+  const password = userId.value;
+  const select = voteOptions.value;
+
+  if (validUserIds.get(user) === password) {
+    if (votedUsers.has(user)) {
+      alert('już głosowałeś');
+    } else {
+      votedUsers.add(user);
+      voteCounts.set(select, voteCounts.get(select) + 1);
+    }
+  } else {
+    alert('nieprawidłowe dane');
+  }
+
   updateResults();
 };
 
 const updateResults = () => {
   let output = '';
-  console.log(output);
   for (const [option, count] of voteCounts) {
-    output += `${option} : ${count} głosów.`;
+    output += `${option} : ${count} głosów.</br>`;
   }
 
   resultDiv.innerHTML = output;
+  userName.value = '';
+  userId.value = '';
+  voteOptions.value = 'Opcja 1';
 };
 
 votingForm.addEventListener('submit', voting);
-
-updateResults();
